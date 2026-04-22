3e37b318555d742faa8188778423a31a6528a7fb
loredanagrmt <giarafran@gmail.com>
2026-04-22 11:53:08 +0200

    WIP:#025: Navegación base

--- app/src/main/java/com/clank/app/MainActivity.java
+++ app/src/main/java/com/clank/app/MainActivity.java
@@ -4,13 +4,14 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.clank.app.util.LanguageManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        LanguageManager.getInstance(this).aplicarIdiomaGuardado();
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-
     }
 }
