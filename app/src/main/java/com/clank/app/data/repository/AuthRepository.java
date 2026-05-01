6089c4214c73647fd1adf3b738d939a644b74bd0
andrea <andreamangasherrera@gmail.com>
2026-05-01 15:39:49 +0200

    END:#07: Actualizados repositorios y FirestoreDataSource para soporte de clanks

--- app/src/main/java/com/clank/app/data/repository/AuthRepository.java
+++ app/src/main/java/com/clank/app/data/repository/AuthRepository.java
@@ -1,11 +1,23 @@
 package com.clank.app.data.repository;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class AuthRepository {
 
+  private final FirebaseAuth auth;
+
   @Inject
-  public AuthRepository() {}
+  public AuthRepository() {
+    this.auth = FirebaseAuth.getInstance();
+  }
+
+  public String getUid() {
+    return auth.getCurrentUser() != null
+            ? auth.getCurrentUser().getUid()
+            : null;
+  }
 }
