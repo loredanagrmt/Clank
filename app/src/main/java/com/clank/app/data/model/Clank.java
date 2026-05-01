965b870a40b64691b435d8cff0b52926384fb4e2
andrea <andreamangasherrera@gmail.com>
2026-05-01 15:31:03 +0200

    END:07:Añadido modelo Clank.java

--- app/src/main/java/com/clank/app/data/model/Clank.java
+++ app/src/main/java/com/clank/app/data/model/Clank.java
@@ -1,10 +1,49 @@
 package com.clank.app.data.model;
 
+import com.google.firebase.firestore.DocumentId;
+import com.google.firebase.firestore.ServerTimestamp;
+
+import java.util.Date;
+import java.util.List;
+
 public class Clank {
+
+  /////////////////////////atributos documento/////////////////////////
+  @DocumentId
+  private String clankId;
+  private String usuarioId;
   private String titulo;
+  private String descripcion;
+  private String portada;
+  private int tiempo;
+  private int numLikes;
+  private boolean estadoAcabado;
+  private List<String> categorias;
+  @ServerTimestamp
+  private Date fechaPublicacion;
+
 
   public Clank() {}
 
-  public String getTitulo()            { return titulo; }
+  /////////////////////////Getters y setters/////////////////////////
+  public String getClankId() { return clankId; }
+  public void setClankId(String clankId) { this.clankId = clankId; }
+  public String getUsuarioId() { return usuarioId; }
+  public void setUsuarioId(String usuarioId) { this.usuarioId = usuarioId; }
+  public String getTitulo() { return titulo; }
   public void setTitulo(String titulo) { this.titulo = titulo; }
+  public String getDescripcion() { return descripcion; }
+  public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
+  public String getPortada() { return portada; }
+  public void setPortada(String portada) { this.portada = portada; }
+  public int getTiempo() { return tiempo; }
+  public void setTiempo(int tiempo) { this.tiempo = tiempo; }
+  public int getNumLikes() { return numLikes; }
+  public void setNumLikes(int numLikes) { this.numLikes = numLikes; }
+  public boolean isEstadoAcabado() { return estadoAcabado; }
+  public void setEstadoAcabado(boolean estadoAcabado) { this.estadoAcabado = estadoAcabado; }
+  public List<String> getCategorias() { return categorias; }
+  public void setCategorias(List<String> categorias) { this.categorias = categorias; }
+  public Date getFechaPublicacion() { return fechaPublicacion; }
+  public void setFechaPublicacion(Date fechaPublicacion) { this.fechaPublicacion = fechaPublicacion; }
 }
