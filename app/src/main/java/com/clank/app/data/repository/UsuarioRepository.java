1daf5b5117596a715dfc2a2ad93d56e0ffa19b69
loredanagrmt <giarafran@gmail.com>
2026-05-06 14:32:05 +0200

    END:REF: UsuarioRepository cambios

--- app/src/main/java/com/clank/app/data/repository/UsuarioRepository.java
+++ app/src/main/java/com/clank/app/data/repository/UsuarioRepository.java
@@ -4,6 +4,7 @@ import com.clank.app.data.model.Usuario;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -13,6 +14,7 @@ import javax.inject.Inject;
 public class UsuarioRepository {
 
     private static final String COLECCION_USUARIOS = "usuarios";
+    private static final String CAMPO_USUARIO_CLANK = "usuarioClank";
 
     private final FirebaseFirestore baseDatos;
 
@@ -21,29 +23,37 @@ public class UsuarioRepository {
         this.baseDatos = FirebaseFirestore.getInstance();
     }
 
-    public Task<DocumentSnapshot> obtenerUsuario(String uid) {
+    public Task<DocumentSnapshot> getUsuario(String uid) {
         return baseDatos
                 .collection(COLECCION_USUARIOS)
                 .document(uid)
                 .get();
     }
 
+    public Task<QuerySnapshot> getUsuariosPorUsuarioClank(String usuarioClank) {
+        return baseDatos
+                .collection(COLECCION_USUARIOS)
+                .whereEqualTo(CAMPO_USUARIO_CLANK, usuarioClank)
+                .limit(1)
+                .get();
+    }
+
     public Task<Void> crear(Usuario usuario) {
         Map<String, Object> datosUsuario = new HashMap<>();
 
-        datosUsuario.put("uid", usuario.obtenerUid());
-        datosUsuario.put("correo", usuario.obtenerCorreo());
-        datosUsuario.put("nombre", usuario.obtenerNombre());
-        datosUsuario.put("telefono", usuario.obtenerTelefono());
-        datosUsuario.put("fotoPerfil", usuario.obtenerFotoPerfil());
-        datosUsuario.put("fechaCreacion", usuario.obtenerFechaCreacion());
-        datosUsuario.put("fechaNacimiento", usuario.obtenerFechaNacimiento());
-        datosUsuario.put("ultimaConexion", usuario.obtenerUltimaConexion());
-        datosUsuario.put("enLinea", usuario.estaEnLinea());
+        datosUsuario.put("uid", usuario.getUid());
+        datosUsuario.put("correo", usuario.getCorreo());
+        datosUsuario.put("nombre", usuario.getNombre());
+        datosUsuario.put("telefono", usuario.getTelefono());
+        datosUsuario.put("fotoPerfil", usuario.getFotoPerfil());
+        datosUsuario.put("fechaCreacion", usuario.getFechaCreacion());
+        datosUsuario.put("fechaNacimiento", usuario.getFechaNacimiento());
+        datosUsuario.put("ultimaConexion", usuario.getUltimaConexion());
+        datosUsuario.put("enLinea", usuario.isEnLinea());
 
         return baseDatos
                 .collection(COLECCION_USUARIOS)
-                .document(usuario.obtenerUid())
+                .document(usuario.getUid())
                 .set(datosUsuario);
     }
 }
